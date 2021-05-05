723d6936837b9b378dc351a00a2b08dfbb03808e
santospanda111 <santospanda111@gmail.com>
2021-05-05 13:35:00 +0530

    added package

---
+++ src/TictactoeGame/TicTacToeGame.java
@@ -0,0 +1,19 @@
+package TictactoeGame;
+
+public class TicTacToeGame {
+    public char board[] = new char[10];//created a tictactoe board of size 10
+
+    TicTacToeGame() {
+        createboard();   //inside the constructor am calling createboard method
+    }
+
+    public void createboard() { //created a method named createboard.
+        for (int count = 1; count < 10; count++) {
+            board[count] = ' ';
+        }
+    }
+
+    public static void main(String[] args) {
+        TicTacToeGame tictactoegame = new TicTacToeGame();
+    }
+}
