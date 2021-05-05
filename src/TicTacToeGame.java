33142cd4f4472b4f9a977145156863d9f073cc48
santospanda111 <santospanda111@gmail.com>
2021-05-05 12:41:18 +0530

    created board for tictactoe game

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,2 +1,14 @@
 public class TicTacToeGame {
+    char board[]=new char[10];
+    TicTacToeGame(){
+        createboard();
+    }
+    void createboard(){
+        for(int count=1;count<10;count++){
+            board[count]=' ';
+        }
+    }
+    public static void main(String[]args){
+        TicTacToeGame component=new TicTacToeGame();
+    }
 }
