e6a3fe3923cf57f6496e74aee33bf23aa1dfb658
santospanda111 <santospanda111@gmail.com>
2021-05-05 14:35:23 +0530

    added uc2 comments

--- src/TictactoeGame/TicTacToeGame.java
+++ src/TictactoeGame/TicTacToeGame.java
@@ -6,27 +6,29 @@ public class TicTacToeGame {
     TicTacToeGame(){
 
         createboard(); //inside the constructor am calling createboard method
-        takeuserinput();
+        takeUserInput();//inside the constructor am calling takeUserInput method
     }
     public void createboard(){  //created a method named createboard.
-        for(int count=1;count<10;count++){
+        for(int count=1;count<10;count++){ //it will iterate this forloop from 1 to 10
             board[count]=' '; //assigned empty space
         }
     }
-    public void takeuserinput(){ //created a method named
+    public void takeUserInput(){ //created a method named
         Scanner Sc=new Scanner(System.in);
         System.out.println("Your turn: ");
         String symbol=Sc.next(); // taking user input to check
-        if (symbol=="x"){
+        if (symbol=="x"){        //Comparing the user input
             usersymbol= "x";
             computersymbol="o";
         }else if (symbol=="o"){
             usersymbol="o";
             computersymbol="x";
+        }else{
+            System.out.println("invalid option");
         }
     }
     public static void main(String[]args){
-        TicTacToeGame component=new TicTacToeGame(); //created object for class TicTacToeGame.
+        TicTacToeGame tictactoe=new TicTacToeGame(); //created object for class TicTacToeGame.
 
     }
 }
