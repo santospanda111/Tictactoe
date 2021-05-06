1f9faa2f1b5f8e1189411055ff87041eba52eac8
santospanda111 <santospanda111@gmail.com>
2021-05-06 11:14:28 +0530

    in UC7 added checkwin method to findout the winner

--- src/TictactoeGame/TicTacToeGame.java
+++ src/TictactoeGame/TicTacToeGame.java
@@ -14,7 +14,7 @@ public class TicTacToeGame {
             board[count]=' '; //assigned empty space
         }
     }
-    public void takeUserInput(){ //created a method named
+    public void takeUserInput(){ //created a method named takeUserInput
         Scanner Sc=new Scanner(System.in);
         System.out.println("Your turn:(Choose x or o) ");
         String symbol=Sc.next(); // taking user input to check
@@ -30,6 +30,7 @@ public class TicTacToeGame {
             System.out.println("invalid option");
         }
     }
+    //this showBoard method is showing the empty board of 3*3.
     public void showBoard(){
         String horizontalPart = "+---+---+---+";
         for (int i = 0; i < 3; i++) {
@@ -42,27 +43,29 @@ public class TicTacToeGame {
         System.out.print(horizontalPart);
         System.out.print("\n");
     }
+    //this makeMove method will take user input from 1 to 9 place your move on the given input.
     public boolean makeMove() {
         Scanner Scan=new Scanner(System.in);
         System.out.println("Make your move by choosing number 1-9 :- ");
         int i=Scan.nextInt();
-        int player=1;
+        int player=1; //initiallized the player with 1
         while (true) {
             if (board[i] == ' ') {
                 if (player == 0) {
                     System.out.println("Computer played: ");
-                    board[i] = computersymbol;
+                    board[i] = computersymbol; //if player==0 then it'll assign the value as given by computer
                 } else {
                     System.out.println("Player played: ");
                     board[i] = usersymbol;
                 }
-                showBoard();
+                showBoard(); //it'll show the position in the board
                 return false;
             } else {
                 return true;
             }
         }
     }
+    //this tossForFirstPlay method will get the input from user and check whether it's a head or tail by using random function
     public void tossForFirstPlay() {
         int player;
         Scanner sc=new Scanner(System.in);
@@ -77,6 +80,74 @@ public class TicTacToeGame {
             player = 0;
         }
     }
+    //this checkWin method will check all the possible winning chances...
+    public void checkWin() {
+        if ((board[1] == usersymbol && board[2] == usersymbol && board[3] == usersymbol)) {
+            System.out.println("Player Wins!");
+            System.exit(0);
+        }
+        if ((board[4] == usersymbol && board[5] == usersymbol && board[6] == usersymbol)) {
+            System.out.println("Player Wins!");
+            System.exit(0);
+        }
+        if ((board[7] == usersymbol && board[8] == usersymbol && board[9] == usersymbol)) {
+            System.out.println("Player Wins!");
+            System.exit(0);
+        }
+        if ((board[1] == usersymbol && board[4] == usersymbol && board[7] == usersymbol)) {
+            System.out.println("Player Wins!");
+            System.exit(0);
+        }
+        if ((board[2] == usersymbol && board[5] == usersymbol && board[8] == usersymbol)) {
+            System.out.println("Player Wins!");
+            System.exit(0);
+        }
+        if ((board[3] == usersymbol && board[6] == usersymbol && board[9] == usersymbol)) {
+            System.out.println("Player Wins!");
+            System.exit(0);
+        }
+        if ((board[1] == usersymbol && board[5] == usersymbol && board[9] == usersymbol)) {
+            System.out.println("Player Wins!");
+            System.exit(0);
+        }
+        if ((board[3] == usersymbol && board[5] == usersymbol && board[7] == usersymbol)) {
+            System.out.println("Player Wins!");
+            System.exit(0);
+        }
+        if ((board[1] == computersymbol && board[2] == computersymbol && board[3] == computersymbol)) {
+            System.out.println("Player Lost!");
+            System.exit(0);
+        }
+        if ((board[4] == computersymbol && board[5] == computersymbol && board[6] == computersymbol)) {
+            System.out.println("Player Lost!");
+            System.exit(0);
+        }
+        if ((board[7] == computersymbol && board[8] == computersymbol && board[9] == computersymbol)) {
+            System.out.println("Player Lost!");
+            System.exit(0);
+        }
+        if ((board[1] == computersymbol && board[4] == computersymbol && board[7] == computersymbol)) {
+            System.out.println("Player Lost!");
+            System.exit(0);
+        }
+        if ((board[2] == computersymbol && board[5] == computersymbol && board[8] == computersymbol)) {
+            System.out.println("Player Lost!");
+            System.exit(0);
+        }
+        if ((board[3] == computersymbol && board[6] == computersymbol && board[9] == computersymbol)) {
+            System.out.println("Player Lost!");
+            System.exit(0);
+        }
+        if ((board[1] == computersymbol && board[5] == computersymbol && board[9] == computersymbol)) {
+            System.out.println("Player Lost!");
+            System.exit(0);
+        }
+        if ((board[3] == computersymbol && board[5] == computersymbol && board[7] == computersymbol)) {
+            System.out.println("Player Lost!");
+            System.exit(0);
+        }
+    }
+
 
     public static void main(String[]args){
         TicTacToeGame tictactoe=new TicTacToeGame(); //created object for class TicTacToeGame
